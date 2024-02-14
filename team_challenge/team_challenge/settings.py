9960a4ac4ebfb87ae50513ffb5890288dc52e759
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-02-14 15:28:03 +0200

    deploy gcp app engine

--- team_challenge/team_challenge/settings.py
+++ team_challenge/team_challenge/settings.py
@@ -1,22 +1,29 @@
 from pathlib import Path
 import os
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'D:\backend\team_challenge\teamchallenge-2105a95396e0.json'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = (
+    str(BASE_DIR) + "/teamchallenge-2105a95396e0.json"
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g%fp$#p^xx8cdkf^4=)qxt=k-gjda+ysq)izxcb(7o=1ei%_cm'
+# SECRET_KEY = 'django-insecure-g%fp$#p^xx8cdkf^4=)qxt=k-gjda+ysq)izxcb(7o=1ei%_cm'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG')
+
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -119,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
