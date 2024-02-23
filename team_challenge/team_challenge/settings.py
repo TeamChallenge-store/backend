b9c22facc273ec063b6825277f88190d41267711
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-02-23 16:41:02 +0200

    permission to access from different domains

--- team_challenge/team_challenge/settings.py
+++ team_challenge/team_challenge/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'django_extensions',
     'storages',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'team_challenge.urls'
@@ -136,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'teamchallenge_backend' 
+
+CORS_ALLOW_ALL_ORIGINS = True
