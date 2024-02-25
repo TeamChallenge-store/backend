d279a580ca37997ee36ce5d6fbe42485d12f940f
OlehVakulchyk <4020405@gmail.com>
2024-02-25 12:50:29 +0200

    an anonymous user's basket has been added
    - an anonymous user's basket has been added
    - implemented deleting an item from the cart
    - implemented random product rating

--- team_challenge/basket/models.py
+++ team_challenge/basket/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.sessions.models import Session
 from django.contrib.auth.models import User
+
 
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -12,7 +14,7 @@ class Brand(models.Model):
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -20,12 +22,10 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # category = models.CharField(max_length=255, default='SSR')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(blank=True)
-    description = models.TextField()
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    # brand = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    brand = models.ForeignKey(Brand, blank=True, on_delete=models.CASCADE)
     quantity_in_stock = models.IntegerField()
     rate = models.IntegerField(default=0)
 
@@ -33,10 +33,23 @@ class Product(models.Model):
         return self.name
 
 class Cart(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null = True, blank = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     products = models.ManyToManyField(Product, through='CartItem')
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
+class CartAnonymous(models.Model):
+    session = models.OneToOneField(Session, on_delete=models.CASCADE, null = True, blank = True, default=None)
+    products = models.ManyToManyField(Product, through="CartAnonymousItem")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+class CartAnonymousItem(models.Model):
+    cart = models.ForeignKey(CartAnonymous, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
--- team_challenge/basket/serializers.py
+++ team_challenge/basket/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, CartItem, Brand, Category
+from .models import Product, CartItem, Brand, Category, CartAnonymousItem
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,3 +31,13 @@ class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ['id', 'product_name', 'quantity', 'product_price', 'product_image']
+
+
+class CartAnonymousItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.ReadOnlyField(source="product.name")
+    product_price = serializers.ReadOnlyField(source="product.price")
+    product_image = serializers.ImageField(source="product.image")
+
+    class Meta:
+        model = CartAnonymousItem
+        fields = ["id", "product_name", "quantity", "product_price", "product_image"]
