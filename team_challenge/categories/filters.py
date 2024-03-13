e22b6421004ce72c2ca9b69afebbbf91cb0ce68d
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-03-13 16:50:53 +0200

    field color as a separate table

--- team_challenge/categories/filters.py
+++ team_challenge/categories/filters.py
@@ -5,7 +5,7 @@ class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
     brand = django_filters.CharFilter(field_name='brand__name', lookup_expr='iexact')
-    color = django_filters.CharFilter(field_name='color', lookup_expr='iexact')
+    color = django_filters.CharFilter(field_name='color__name', lookup_expr='iexact')
 
     class Meta:
         model = Product
--- team_challenge/products/models.py
+++ team_challenge/products/models.py
@@ -10,6 +10,12 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
+class Color(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -21,7 +27,7 @@ class Product(models.Model):
     brand = models.ForeignKey(Brand, blank=True, on_delete=models.CASCADE)
     quantity_in_stock = models.IntegerField()
     rate = models.IntegerField(default=0)
-    color = models.CharField(max_length=50, blank=True)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
