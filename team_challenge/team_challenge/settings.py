9c2eb8a5dc0e16d6185223569900aae53e33b500
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-04-29 21:21:16 +0300

    add cloud storage

--- team_challenge/team_challenge/settings.py
+++ team_challenge/team_challenge/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = 'oleksandr'
+GS_BUCKET_NAME = 'bucket_photos1'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
